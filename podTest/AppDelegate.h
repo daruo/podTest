de97834305d12a36358d3570c58e0353cdd552f4
daruo <daruo258@163.com>
2018-06-26 10:55:57 +0800

    初始化工程

---
+++ podTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podTest
+//
+//  Created by Daruo on 2018/6/26.
+//  Copyright © 2018年 BeeSmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ podTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  podTest
+//
+//  Created by Daruo on 2018/6/26.
+//  Copyright © 2018年 BeeSmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
